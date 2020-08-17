01c72dab5f3220e7d219a377340717a06073fffb
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-17 16:28:16 -0400

    Testing App

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders app without crashing', () => {
+    render(
+        <App />
+      );
+      expect(screen.getByText(/government experiments/i)).toBeInTheDocument();
+    
+
+});
